7cf5dae161247f0df3ad9730934e6c1a4d3deac0
Juliette Audema <juliette.audema@gmail.com>
2018-10-26 01:00:37 +0200

    Ajout de chiens aux strolls

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,22 @@
 
 require 'faker'
 
-10.times do |city|
+15.times do |city|
 	cities = City.create!(city_name: Faker::GameOfThrones.city)
 end
 
-10.times do |index|
+15.times do |index|
 	dogsitters = Dogsitter.create!(name: Faker::Superhero.name, city_id: Faker::Number.between(1,10))
 	dogs = Dog.create!(name: Faker::TwinPeaks.character, specie: Faker::Dessert.topping, city_id: Faker::Number.between(1,10))
 end
 
-10.times do |index|
+15.times do |index|
 	strolls = Stroll.create!(date: "2018111 #{Faker::Number.between(5, 23)}:#{Faker::Number.between(1, 59)}:00", dogsitter_id: Faker::Number.between(1,10))
+end
+
+i = 1
+while i < 11
+Stroll.find(i).dogs <<  Dog.find(i)
+Stroll.find(i).dogs <<  Dog.find(i+1)
+i = i+1
 end
